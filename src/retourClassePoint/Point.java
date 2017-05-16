9ba2086f2a47bb98753b01ee188501b2f1f76b99
StraetenMax <maxstraeten@yahoo.com>
2017-05-16 11:03:27 +0200

    classe Point propriétés constructeurs mutateur

--- src/retourClassePoint/Point.java
+++ src/retourClassePoint/Point.java
@@ -2,4 +2,31 @@ package retourClassePoint;
 
 public class Point {
 	//propriétés
+	private double positionX;
+	private double positionY;
+	
+	//constructeurs vide et initialisé
+	public Point(){
+		positionX = 00.00;
+		positionY = 00.00;
+	}
+	public Point(double positionX, double positionY){
+		this.positionX = positionX;
+		this.positionY = positionY;
+	}
+	
+	//accesseurs et mutateurs
+	public double getPositionX(){
+		return positionX;
+	}
+	public void setPositionX(double positionX){
+		this.positionX = positionX;
+	}
+	public double getPositionY(){
+		return positionY;	
+	}
+	public void setPositionY(double positionY){
+		this.positionY = positionY;
+		
+	}
 }
